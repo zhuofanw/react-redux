ec99dc2f33e4766d802ea1216506a420b38cf3bf
zhuofanw <zhuofanw@sfu.ca>
2019-03-11 22:44:54 -0700

    basic html generated

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="container">
+          <h1 className="jumbotron-heading text-center">1</h1>
+          <p className="text-center">
+              <button className="btn btn-primary mr-2">Increase</button>
+              <button className="btn btn-primary mr-2">Decrease</button>
+          </p>
+        </div>
+    );
+  }
+}
+
+export default App;
