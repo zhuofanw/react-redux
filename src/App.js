8ea9cff39fce5a849ab34f77a30b417b4ec5bfe4
zhuofanw <zhuofanw@sfu.ca>
2019-03-12 16:49:25 -0700

    dispatch increment/decrement way 2 (using mapDispatchToProps)

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import { connect } from 'react-redux';
 import { increment, decrement } from './actions';
 class App extends Component {
   render() {
-    const { dispatch } = this.props;
-    console.dir(this.props)
+    const { increment } = this.props;
     return (
         <div className="container">
           <h1 className="jumbotron-heading text-center">{ this.props.counter }</h1>
           <p className="text-center">
-              <button onClick={ () => dispatch(increment())} className="btn btn-primary mr-2">Increase</button>
-              <button onClick={ () => dispatch(decrement())} className="btn btn-danger mr-2">Decrease</button>
+              <button onClick={ () => increment("zhuofan")} className="btn btn-primary mr-2">Increase</button>
+              <button className="btn btn-danger mr-2">Decrease</button>
           </p>
         </div>
     );
@@ -25,8 +24,14 @@ const mapStateToProps = (state) =>{
   };
 }
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: (name)=> { dispatch(increment(name)) }
+  }
+}
+
 App.propTypes = {
   counter: PropTypes.number.isRequired
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
